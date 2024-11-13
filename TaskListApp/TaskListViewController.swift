b48dfc6a651adff2e6aace853b81cfa3bef1260d
Evgeniy <borovoyevgen@gmail.com>
2024-11-13 16:44:33 +0400

    Create and set Appearance for Navigation bar

--- TaskListApp/TaskListViewController.swift
+++ TaskListApp/TaskListViewController.swift
@@ -12,9 +12,47 @@ final class TaskListViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        setupNavigationBar()
+    }
+    
+    @objc private func addTask() {
+        // TODO: create VC for add task
+    }
+    
+    
+}
+// MARK: - Setup UI
+private extension TaskListViewController {
+    func setupNavigationBar() {
+        title = "Task List"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        // Navigation bar Appearance
+        let navBarAppearance = UINavigationBarAppearance()
+        navBarAppearance.backgroundColor = UIColor(
+            red: 0,
+            green: 1,
+            blue: 1,
+            alpha: 1
+        )
+        
+        navBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.purple]
+        navBarAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.purple]
+        
+        // Button for navigation bar
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .add,
+            target: self,
+            action: #selector(addTask)
+        )
+        
+        navigationController?.navigationBar.tintColor = UIColor.purple
+        
+        // Apply appearance
+        navigationController?.navigationBar.standardAppearance = navBarAppearance
+        navigationController?.navigationBar.scrollEdgeAppearance = navBarAppearance
         
     }
-
-
+    
 }
 
