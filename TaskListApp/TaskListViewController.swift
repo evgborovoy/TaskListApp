bc8a84c99a070e6464afaf43486e08597275589b
Evgeniy <borovoyevgen@gmail.com>
2024-11-13 15:46:23 +0400

    Initial Commit

---
+++ TaskListApp/TaskListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TaskListApp
+//
+//  Created by Evgeniy Borovoy on 11/13/24.
+//
+
+import UIKit
+
+final class TaskListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        
+    }
+
+
+}
+
